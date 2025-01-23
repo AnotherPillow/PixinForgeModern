d67a72df571185331c6d70f28a3be7af02282553
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-01-23 18:48:08 +0000

    Fix for 1.3.5.6

--- src/main/java/com/anotherpillow/pixinforgemodern/mixins/EssentialConnectionMixin.java
+++ src/main/java/com/anotherpillow/pixinforgemodern/mixins/EssentialConnectionMixin.java
@@ -12,12 +12,26 @@ import java.net.URI;
 public class EssentialConnectionMixin {
     @SuppressWarnings("UnresolvedMixinReference")
     @Redirect(
-            method = "<init>",
+            method = "<clinit>",
             at = @At(value = "INVOKE", target = "java/net/URI.create(Ljava/lang/String;)Ljava/net/URI;")
     )
     private static URI redirectUriCreate(String originalUri) {
         String modifiedUri = originalUri.replace("essential.gg", "pixie.rip");
         System.out.println("[Pixin] Original URI: " + originalUri + " , replacing with " + modifiedUri);
+        System.out.println("[Pixin] Ignore the below warning from Essential. It may be ignored");
+        return URI.create(modifiedUri);
+    }
+
+    // just in case the user is running an old version of essential too (e.g. embedded as a library)
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Redirect(
+            method = "<init>",
+            at = @At(value = "INVOKE", target = "java/net/URI.create(Ljava/lang/String;)Ljava/net/URI;")
+    )
+    private static URI redirectUriCreateOld(String originalUri) {
+        String modifiedUri = originalUri.replace("essential.gg", "pixie.rip");
+        System.out.println("[Pixin] Original URI: " + originalUri + " , replacing with " + modifiedUri);
+        System.out.println("[Pixin] Ignore the below warning from Essential. It may be ignored");
         return URI.create(modifiedUri);
     }
 }
